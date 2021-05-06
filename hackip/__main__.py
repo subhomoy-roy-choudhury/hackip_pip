e79816d3ae09db0dd5d186a0cccec29f717cacd8
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2021-05-06 14:51:34 +0530

    changes

---
+++ hackip/__main__.py
@@ -0,0 +1,7 @@
+def main():
+    number = int(input('Enter the number (only positive integers allowed\n'))
+    print(f'{number} squares is {number**2}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ test.py
@@ -0,0 +1,3 @@
+from hackip import *
+
+print(main())
