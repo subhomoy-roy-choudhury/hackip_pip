03f79234f86cfa59cb6f3342557caccca6001ed0
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2021-05-07 14:46:04 +0530

    Added Readme

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="Hack_IP",
-    version="1.0.7",
+    version="1.0.8",
     description= DESCRIPTION,
     long_description = README,
     long_description_content_type="text/markdown",
